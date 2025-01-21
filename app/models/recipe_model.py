7d318500e587cd1a85bfa2e664efce0a234e6861
Sam Woodbeck <samuel.woodbeck@gmail.com>
2025-01-21 11:28:19 -0600

    Updated sqlalchemy models to use Declarative Mapping

--- app/models/recipe_model.py
+++ app/models/recipe_model.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from sqlalchemy import Column, Integer, String, Text
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
     pass
@@ -7,10 +8,10 @@ class Base(DeclarativeBase):
 class Recipe(Base):
     __tablename__ = "recipes"
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(Text)
-    ingredients = Column(Text)
-    steps = Column(Text)
-    cooking_time = Column(Integer) # in minutes
-    servings = Column(Integer)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(index=True)
+    description: Mapped[str] = mapped_column(Text)
+    ingredients: Mapped[str] = mapped_column(Text)
+    steps: Mapped[str] = mapped_column(Text)
+    cooking_time: Mapped[int] = mapped_column()
+    servings: Mapped[int] = mapped_column()
